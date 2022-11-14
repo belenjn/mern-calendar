7c59bcb94a2ea664d57255ffa235aeede4751906
Belen JN <belenjarabanavas31@gmail.com>
2022-11-14 16:18:47 +0100

    New height in calendar

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,17 +1,43 @@
 import { Navbar } from '../'
 import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
-import format from 'date-fns/format'
-import parse from 'date-fns/parse'
-import startOfWeek from 'date-fns/startOfWeek'
-import getDay from 'date-fns/getDay'
-import enUS from 'date-fns/locale/en-US'
+import "react-big-calendar/lib/css/react-big-calendar.css"
+import { addHours, format, parse, startOfWeek, getDay } from 'date-fns';
+import { enUS } from 'date-fns/locale';
 
+const locales = {
+  'en-US': enUS,
+}
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+})
+
+const events = [{
+  title: 'Cumpleaños del jefe',
+  notes: 'Hay que comprar el pastel',
+  start: new Date(),
+  end: addHours(new Date(), 2),
+  user: {
+    _id: '123',
+    name: 'Belen'
+  }
+}]
 
 export const CalendarPage = () => {
   return (
     <>
     <Navbar/>
-    <Calendar></Calendar>
+    <Calendar
+      localizer={localizer}
+      events={events}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: "calc(100vh - 80px)" }}
+    />
     </>
   )
 }
