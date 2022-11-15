0a575abf56b49bc932478860e6cdff7c57b99c98
Belen JN <belenjarabanavas31@gmail.com>
2022-11-15 10:24:43 +0100

    getMessages created

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,32 +1,35 @@
-import { Navbar } from '../'
-import { Calendar } from 'react-big-calendar'
-import "react-big-calendar/lib/css/react-big-calendar.css"
-import { addHours } from 'date-fns';
-import { localizer } from '../../helpers';
+import { Navbar } from "../";
+import { Calendar } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { addHours } from "date-fns";
+import { localizer, getMessagesES } from "../../helpers";
 
-
-const events = [{
-  title: 'Cumpleaños del jefe',
-  notes: 'Hay que comprar el pastel',
-  start: new Date(),
-  end: addHours(new Date(), 2),
-  user: {
-    _id: '123',
-    name: 'Belen'
-  }
-}]
+const events = [
+  {
+    title: "Cumpleaños del jefe",
+    notes: "Hay que comprar el pastel",
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    user: {
+      _id: "123",
+      name: "Belen",
+    },
+  },
+];
 
 export const CalendarPage = () => {
   return (
     <>
-    <Navbar/>
-    <Calendar
-      localizer={localizer}
-      events={events}
-      startAccessor="start"
-      endAccessor="end"
-      style={{ height: "calc(100vh - 80px)" }}
-    />
+      <Navbar />
+      <Calendar
+        culture="es"
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: "calc(100vh - 80px)" }}
+        messages={getMessagesES()}
+      />
     </>
-  )
-}
+  );
+};
